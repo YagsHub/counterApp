0680a11996ef81a05e7e063563bd91d1dd5bbf79
YagsHub <167205651+YagsHub@users.noreply.github.com>
2024-07-14 00:09:56 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,19 +1,14 @@
-// document.getElementById("count-el").innerText = 5;
 
-// intialize the count as 0
-// listen for clicks on the increment button
-// increment the count variable when the button is clicked
-// change the count-el in the HTML to reflect the new count
 let countElement = document.getElementById("count-el");
 let previousElement = document.getElementById("previous");
 let count = 0; 
 
 function increment() {
     count += 1; 
-    countElement.innerText = count;
+    countElement.textContent = count;
 }
-// 1. Create a function, save(), which logs out the count when it's called
+
 function save(){
     let countStr = count + " - ";
-    previousElement.innerText += countStr;
+    previousElement.textContent += countStr;
 }
